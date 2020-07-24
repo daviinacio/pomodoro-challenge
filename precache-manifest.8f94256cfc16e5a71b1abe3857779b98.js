94cce34f63bbf3ad62b0d0e3dd77e60b567d92a1
Davi Inácio <aazz6850@gmail.com>
2020-07-24 15:35:09 -0300

    Updates

---
+++ precache-manifest.8f94256cfc16e5a71b1abe3857779b98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6fcbe8affc19fa81b800583ac8a4c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f633f3556beda8a05e88",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a11072a7895d857f432b",
+    "url": "/static/js/2.7268a58e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7268a58e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f633f3556beda8a05e88",
+    "url": "/static/js/main.51480b79.chunk.js"
+  },
+  {
+    "revision": "343bc61d20d623f43a65",
+    "url": "/static/js/runtime-main.6b805be8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
