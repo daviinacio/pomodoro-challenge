3696c368113cca767badbe93a69d9d10a26aac9a
Davi Inácio <aazz6850@gmail.com>
2020-08-04 23:04:36 -0300

    Updates

---
+++ precache-manifest.402d2edb0fe8468af13e5ad68069c593.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffbf6bdf91dc7aab07c769c802014f5d",
+    "url": "https://pomodoro.app.br/index.html"
+  },
+  {
+    "revision": "642b551298aae53c55c2",
+    "url": "https://pomodoro.app.br/static/css/main.2368eb95.chunk.css"
+  },
+  {
+    "revision": "e2910a85f138623d9f87",
+    "url": "https://pomodoro.app.br/static/js/2.15b7be95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://pomodoro.app.br/static/js/2.15b7be95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d83ede7785f05408100",
+    "url": "https://pomodoro.app.br/static/js/3.da8769b1.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "https://pomodoro.app.br/static/js/3.da8769b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "642b551298aae53c55c2",
+    "url": "https://pomodoro.app.br/static/js/main.f3c17bbf.chunk.js"
+  },
+  {
+    "revision": "a6f40db8dd443e8ce127",
+    "url": "https://pomodoro.app.br/static/js/runtime-main.e63b5b7a.js"
+  },
+  {
+    "revision": "5cf7421f1b3a4f58187b5bbabb5f0234",
+    "url": "https://pomodoro.app.br/static/media/end_of_long_pause.5cf7421f.mp3"
+  },
+  {
+    "revision": "dbe9517a90f17e19ff9a24dd9ef7f377",
+    "url": "https://pomodoro.app.br/static/media/end_of_pause.dbe9517a.mp3"
+  },
+  {
+    "revision": "a31529e437e258d3477d9ec0383a0512",
+    "url": "https://pomodoro.app.br/static/media/end_of_work.a31529e4.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2b1b859bafdf722bd9419d09a1fbcd9.js"
+  "https://pomodoro.app.br/precache-manifest.402d2edb0fe8468af13e5ad68069c593.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://pomodoro.app.br/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
